56f2840f56f1672fe0d3d205eb954237eccdfe87
trustytrojan <tarbot24@hotmail.com>
2024-02-06 10:11:02 -0500

    better deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-[ ! -d node_modules ] && npm i
-[ ! $1 ] && (echo "Port required"; exit 1)
-kill $(cat pid) 2>/dev/null
+[ -d node_modules ] || npm i
+[ $1 ] || { echo "Port required"; exit 1; }
+kill $(<pid) 2>/dev/null
 node . $1 &>log & echo $! >pid
