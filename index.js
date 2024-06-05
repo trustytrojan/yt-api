5dc0c4f9e9303da4a3348583559492bda64d9d37
trustytrojan <tarbot24@hotmail.com>
2024-06-04 21:57:35 -0400

    small fix to prevent crashing on limited storage servers: videos will no longer be cached on disk, but audio files will

--- index.js
+++ index.js
@@ -40,14 +40,16 @@ app.get('/yt/dl/:idOrUrl',
 		// strip title of header-incompatible characters
 		const strippedTitle = details.title.replaceAll(/:|[^\x20-\x7F]/g, '');
 
-		const onlyOneAudio = (formats.length === 1) && formats[0].hasAudio && !formats[0].hasVideo;
+		const onlyAudio = (formats.length === 1) && formats[0].hasAudio && !formats[0].hasVideo;
 
-		if (!onlyOneAudio && details.lengthSeconds > 3_600)
+		if (!onlyAudio && details.lengthSeconds > 3_600)
+			// reject videos over 1 hour long
+			// TODO: make an error.html page (with styles) instead of sending text
 			return res.status(400).send('video too long!\r\n');
 
-		// if formats contains only one audio format, use mp3
-		res.setHeader('Content-Type', onlyOneAudio ? 'audio/mp3' : 'video/x-matroska');
-		res.setHeader('Content-Disposition', `attachment; filename="${strippedTitle}.${onlyOneAudio ? 'mp3' : 'mkv'}"`);
+		// use mp3 for audio-only downloads
+		res.setHeader('Content-Type', onlyAudio ? 'audio/mp3' : 'video/x-matroska');
+		res.setHeader('Content-Disposition', `attachment; filename="${strippedTitle}.${onlyAudio ? 'mp3' : 'mkv'}"`);
 
 		// asynchronously delete old files (oldness defined in cache.js)
 		cache.deleteOldFiles();
@@ -58,9 +60,12 @@ app.get('/yt/dl/:idOrUrl',
 			return res.sendFile(cachePath);
 
 		// start ffmpeg and begin streaming
-		const ffmpeg = util.spawnFfmpeg(formats.map(f => f.url), details, onlyOneAudio ? 'mp3' : null);
+		const ffmpeg = util.spawnFfmpeg(formats.map(f => f.url), details, onlyAudio ? 'mp3' : null);
 		ffmpeg.stdout.pipe(res);
-		ffmpeg.stdout.pipe(createWriteStream(cachePath));
+
+		if (onlyAudio)
+			// only cache audio-only media... my vps isn't big enough for caching potentially large videos
+			ffmpeg.stdout.pipe(createWriteStream(cachePath));
 	}
 );
 
